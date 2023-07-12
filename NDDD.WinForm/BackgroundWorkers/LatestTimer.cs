9ea2988d71a61a02ac2417570be6027417845d55
yo1030 <0vant1c@gmail.com>
2023-07-12 20:36:54 +0900

    timer クラス

---
+++ NDDD.WinForm/BackgroundWorkers/LatestTimer.cs
@@ -0,0 +1,43 @@
+﻿namespace NDDD.WinForm.BackgroundWorkers
+{
+    internal static class LatestTimer
+    {
+        private static System.Threading.Timer _timer;
+        private static bool _isWork = false;
+
+        static LatestTimer()
+        {
+            _timer = new System.Threading.Timer(Callback);
+        }
+
+        internal static void Start()
+        {
+            _timer.Change(10000, 10000);
+        }
+
+        internal static void Stop()
+        {
+            _timer.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+
+        private static void Callback(object o)
+        {
+            //_timer.Change(Timeout.Infinite, Timeout.Infinite);
+
+            if (_isWork)
+            {
+                return;
+            }
+
+            try
+            {
+                _isWork = true;
+            }
+            finally
+            {
+                _isWork = false;
+                //_timer.Change(10000, 10000);
+            }
+        }
+    }
+}
